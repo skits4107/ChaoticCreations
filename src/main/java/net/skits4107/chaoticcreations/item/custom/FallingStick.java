f345172be74d11fba9593dd8ada8c9ba991a1206
aaron <aaronjosua719@gmail.com>
2023-12-26 18:49:24 -0800

    falling stick AOE and durability

--- src/main/java/net/skits4107/chaoticcreations/item/custom/FallingStick.java
+++ src/main/java/net/skits4107/chaoticcreations/item/custom/FallingStick.java
@@ -1,10 +1,13 @@
 package net.skits4107.chaoticcreations.item.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
@@ -21,12 +24,49 @@ public class FallingStick extends Item {
         Level level = pContext.getLevel();
         //Player player = pContext.getPlayer();
         BlockState state = level.getBlockState(pos);
-
         if (!level.isClientSide){
             level.setBlock(pos, Blocks.AIR.defaultBlockState(), 3);
             FallingBlockEntity.fall(level, pos, state);
+            pContext.getItemInHand().hurtAndBreak(1, pContext.getPlayer(), (p) -> {
+                p.broadcastBreakEvent(pContext.getHand());
+            });
             return InteractionResult.SUCCESS;
         }
-        return InteractionResult.PASS;
+        return InteractionResult.FAIL;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        if (!level.isClientSide){
+            findBlocksAroundPlayer(player, level,10);
+            player.getItemInHand(usedHand).hurtAndBreak(1, player, (p) -> {
+                p.broadcastBreakEvent(usedHand);
+            });
+        }
+        return super.use(level, player, usedHand);
+    }
+
+    public void findBlocksAroundPlayer(Player player, Level level, int radius) {
+        BlockPos playerPos = player.blockPosition();
+
+        for (int x = -radius; x <= radius; x++) {
+            for (int y = -radius; y <= radius; y++) {
+                for (int z = -radius; z <= radius; z++) {
+                    BlockPos blockPos = playerPos.offset(x, y, z);
+                    BlockState blockState = level.getBlockState(blockPos);
+                    // Process the block as needed
+
+                    if (blockPos.distToCenterSqr(playerPos.getX(), playerPos.getY(), playerPos.getZ()) <= radius*radius){
+                        level.setBlock(blockPos, Blocks.AIR.defaultBlockState(), 3);
+                        FallingBlockEntity.fall(level, blockPos, blockState);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean isDamageable(ItemStack stack) {
+        return true;
     }
 }
